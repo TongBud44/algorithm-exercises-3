2a5f5675cc9327ff61db2a04a6e133a8a65ffa75
tong <reangsak1234@hotmail.com>
2024-12-19 20:48:28 +0700

    feat: algorithm exercise 3 answer

--- ex-1.js
+++ ex-1.js
@@ -1,11 +1,20 @@
 function findStudentIndex(students, searchStudent) {
   // Start coding here
+  for (let i = 0; i < students.length; i++) {
+    if (students[i] === searchStudent) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /* 
   1. ควรใช้ Search Algorithm ใดในการแก้โจทย์นี้ 
+      Answer: linear search
   2. ให้เหตุผลประกอบว่าทำไมถึงเลือกใช้ Search Algorithm นี้
+      Answer: เพราะ Array ที่ให้มาไม่ได้เรียงลำดับข้อมูล
   3. ให้ระบุว่า Big O Notation ของ Function นี้คือเท่าไร เพราะอะไร
+      Answer: O(n) เพราะ จำนวนครั้งการลูปขึ้นอยู่กับขนาดของข้อมูล อย่างข้อนี้ต้องลูป 8 ครั้งถึงจะเจอ John
 */
 
 const students = [
--- ex-2.js
+++ ex-2.js
@@ -1,11 +1,29 @@
 function findBookIndex(books, searchBook) {
   // Start coding here
+  let left = 0;
+  let right = books.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (books[mid].title === searchBook) {
+      return mid;
+    } else if (books[mid].title < searchBook) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
 
 /*
   1. ควรใช้ Search Algorithm ใดในการแก้โจทย์นี้ 
+      Answer: binary search
   2. ให้เหตุผลประกอบว่าทำไมถึงเลือกใช้ Search Algorithm นี้
+      Answer: เพราะ Array ที่ให้มาเรียงลำดับข้อมูลตามตัวอักษร
   3. ให้ระบุว่า Big O Notation ของ Function นี้คือเท่าไร เพราะอะไร
+      Answer: O(log n) เพราะ ทุกครั้งที่ลูปขอบเขตการหาจะลดลงครึ่งหนึ่ง
 */
 
 const books = [
